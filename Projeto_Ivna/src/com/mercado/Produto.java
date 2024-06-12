7c7556ad07e9b476f511c2ae3dc912048d3f73f4
joaocarloss112 <135226719+joaocarloss112@users.noreply.github.com>
2024-06-12 15:29:10 -0300

    Produto.java

--- Projeto_Ivna/src/com/mercado/Produto.java
+++ Projeto_Ivna/src/com/mercado/Produto.java
@@ -1,42 +1,67 @@
 package com.mercado;
 
 public class Produto {
-	    private String nome;
-	    private double preco;
-	    private int quantidade;
+    private String codigo;
+    private String nome;
+    private double precoVenda;
+    private double custo;
+    private int quantidade;
+    private int quantidadeVendida;
 
-	    public Produto(String nome, double preco, int quantidade) {
-	        this.nome = nome;
-	        this.preco = preco;
-	        this.quantidade = quantidade;
-	    }
+    public Produto(String codigo, String nome, double precoVenda, double custo, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.precoVenda = precoVenda;
+        this.custo = custo;
+        this.quantidade = quantidade;
+        this.quantidadeVendida = 0;
+    }
 
-	    // Getters e Setters
-	    public String getNome() {
-	        return nome;
-	    }
+    public String getCodigo() {
+        return codigo;
+    }
 
-	    public void setNome(String nome) {
-	        this.nome = nome;
-	    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 
-	    public double getPreco() {
-	        return preco;
-	    }
+    public String getNome() {
+        return nome;
+    }
 
-	    public void setPreco(double preco) {
-	        this.preco = preco;
-	    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	    public int getQuantidade() {
-	        return quantidade;
-	    }
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
 
-	    public void setQuantidade(int quantidade) {
-	        this.quantidade = quantidade;
-	    }
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
 
+    public double getCusto() {
+        return custo;
+    }
 
-	}
+    public void setCusto(double custo) {
+        this.custo = custo;
+    }
 
+    public int getQuantidade() {
+        return quantidade;
+    }
 
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getQuantidadeVendida() {
+        return quantidadeVendida;
+    }
+
+    public void incrementarQuantidadeVendida(int quantidadeVendida) {
+        this.quantidadeVendida += quantidadeVendida;
+    }
+}
